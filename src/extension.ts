5b0980a119e706a0be1dfcd4343f552ae0033724
Neo <neo@sagittal.ai>
2025-05-23 11:13:45 +0000

    Add line change tracker to status bar
    Implemented a VS Code extension feature that tracks the number of
    lines added and deleted in the current unstaged and staged changes.
    The line change summary is displayed on the status bar for easy
    visibility. This was achieved using the 'simple-git' library to
    fetch the git diff summary and dynamically update the status bar
    with the line changes. Additionally, a command 'difflite.showLineChanges'
    was added to manually refresh the line change summary.
    
    Fixes #1

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as simpleGit from 'simple-git';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,16 +11,38 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "difflite" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('difflite.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from DiffLite!');
+	// Create a status bar item
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBarItem.text = 'Loading changes...';
+	statusBarItem.show();
+
+	// Function to update the status bar with line changes
+	async function updateLineChanges() {
+		try {
+			const git = simpleGit();
+			const diffSummary = await git.diffSummary();
+
+			const added = diffSummary.insertions;
+			const deleted = diffSummary.deletions;
+
+			statusBarItem.text = `+${added} -${deleted} lines`;
+		} catch (error) {
+			console.error('Error fetching git diff:', error);
+			statusBarItem.text = 'Error fetching changes';
+		}
+	}
+
+	// Update line changes on activation
+	updateLineChanges();
+
+	// Register the command to manually refresh line changes
+	const disposable = vscode.commands.registerCommand('difflite.showLineChanges', () => {
+		updateLineChanges();
+		vscode.window.showInformationMessage('Line changes updated!');
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(statusBarItem);
 }
 
 // This method is called when your extension is deactivated
